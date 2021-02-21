88bf1cb22c45ef27ff9bf3d6f506e948ba8bca05
Deepak Kumar Dash <dipudash.2003@gmail.com>
2021-02-21 09:34:08 +0530

    Added function for calculating distance image of point and foot of perpendicular

--- main.py
+++ main.py
@@ -47,15 +47,13 @@ class line:
             raise Exception("Both points must be different to make a line")
         elif p1.x == p2.x:
             self.slope = math.inf
-            self.a = 0
-            self.b = 1
+            self.a = 1
+            self.b = 0
             self.c = -p1.x
-
-
         else:
             self.slope = m = (p2.y - p1.y) / (p2.x - p1.x)
-            self.a = 1
-            self.b = -m
+            self.a = -m
+            self.b = 1
             self.c = (m * p1.x) - p1.y
 
     def __str__(self):
@@ -112,8 +110,49 @@ class line:
             angle = math.degrees(angle)
             return angle
 
-     
-#Testing 
+    def distance(self, pt):
+        """
+        :type pt : point
+        """
+        d = abs(self.a * pt.x + self.b * pt.y + self.c) / math.sqrt((self.a ** 2) + (self.b ** 2))
+        return d
+
+    def imageOfPointThroughLine(pt,ln):
+        """
+        :type pt : point
+        :type ln : line
+        """
+        x=y=0
+        x1=pt.x
+        y1=pt.y
+        a=ln.a
+        b=ln.b
+        c=ln.c
+        x=x1-((2*a*(a*x1 + b*y1 + c))/((a**2) + (b**2)))
+        y = y1 - ((2 * b * (a * x1 + b * y1 + c)) / ((a ** 2) + (b ** 2)))
+        image=point(x,y)
+        return image
+
+    def footOfPerpendicular(pt, ln):
+        """
+        :type pt : point
+        :type ln : line
+        """
+        x = y = 0
+        x1 = pt.x
+        y1 = pt.y
+        a = ln.a
+        b = ln.b
+        c = ln.c
+        x = x1 - ((a * (a * x1 + b * y1 + c)) / ((a ** 2) + (b ** 2)))
+        y = y1 - ((b * (a * x1 + b * y1 + c)) / ((a ** 2) + (b ** 2)))
+        footOfPerpendicular = point(x, y)
+        return footOfPerpendicular
+
+
+
+
+# Testing
 
 p1 = point(1, 0)
 p2 = point(2, 0)
@@ -125,3 +164,15 @@ l2 = line(p2, p3)
 print(l1.angleBetweenLinesInDegrees(l0))
 print(l1.angleBetweenLinesInRadians(l0))
 print(l2.angleBetweenLinesInDegrees(l2))
+print(l2.isParallel(l2))
+XAXIS = line(point(0, 0), point(1, 0))
+YAXIS = line(point(0, 0), point(0, 1))
+X = XAXIS
+Y = YAXIS
+print(XAXIS.distance(point(0, 3)))
+print(XAXIS)
+print(YAXIS)
+l3=line(point(3,4),point(3,5))
+print(line.footOfPerpendicular(point(4,4),l3))
+print(line.imageOfPointThroughLine(point(4,4),l3))
+
